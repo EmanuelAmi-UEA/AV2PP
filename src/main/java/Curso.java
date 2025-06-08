32e7349002add5683e8bfc4ad8801cca09d99102
Julio Cleiton Seabra Mendes <jcsme.snf24@uea.edu.br>
2025-06-07 21:01:05 -0300

    Adicionado todas as classes da pp1 e incluido a super classe Pessoa

---
+++ src/main/java/Curso.java
@@ -0,0 +1,56 @@
+import java.util.ArrayList;
+
+public class Curso{
+  private String nome;
+  private String cargaHoraria;
+  private String ementa;
+  private Professor professor;
+  private ArrayList<Turma> turmas;
+
+  public Curso(String nome, String cargaHoraria, String ementa){
+    this.nome = nome;
+    this.cargaHoraria = cargaHoraria;
+    this.ementa = ementa;
+    this.turmas = new ArrayList<Turma>();
+  }
+
+  /////////////// Metodos de manipulação de turmas ///////////////////
+  public void adicionarTurma(Turma turma){
+    this.turmas.add(turma);
+  }
+  public void removerTurma(Turma turma){
+    this.turmas.remove(turma);
+  }
+  
+  /////////////// Metodos getters e setters ///////////////////
+  public ArrayList<Turma> getTurmas(){
+    return this.turmas;
+  }    
+  
+  public Professor getProfessor() {
+    return this.professor;
+  }
+
+  public void setProfessor(Professor professor) {
+    this.professor = professor;
+  }
+
+  public String getNome(){
+    return this.nome;
+  }
+  public String getCargaHoraria(){
+    return this.cargaHoraria;
+  }
+  public String getEmenta(){
+    return this.ementa;
+  }
+  public void setNome(String nome){
+    this.nome = nome;
+  }
+  public void setCargaHoraria(String cargaHoraria){
+    this.cargaHoraria = cargaHoraria;
+  }
+  public void setEmenta(String ementa){
+    this.ementa = ementa;
+  }
+}
---
+++ src/main/java/Turma.java
@@ -0,0 +1,50 @@
+import java.util.ArrayList;
+
+public class Turma{
+  private String codigo;
+  private String periodo;
+  private ArrayList<Aluno> listaDeAlunos;
+  private ArrayList<Assessment> listaDeAvaliacoes;
+
+  public Turma(String codigo, String periodo){
+    this.codigo = codigo;
+    this.periodo = periodo;
+    this.listaDeAlunos = new ArrayList<Aluno>();
+    this.listaDeAvaliacoes = new ArrayList<Assessment>();
+  }
+  /////////// Metodos getters e setters ///////////////////
+  public String getCodigo(){
+    return this.codigo;
+  }
+  public String getPeriodo(){
+    return this.periodo;
+  }
+  public ArrayList<Aluno> getListaDeAlunos(){
+    return this.listaDeAlunos;
+  }
+  public void setCodigo(String codigo){
+    this.codigo = codigo;
+  }
+  public void setPeriodo(String periodo){
+    this.periodo = periodo;
+  }
+  public ArrayList<Assessment> getListaDeAvaliacoes(){
+    return this.listaDeAvaliacoes;
+  }
+  ///////// Metodos de manipulação de alunos /////////
+  public void adicionarAluno(Aluno aluno){
+    this.listaDeAlunos.add(aluno);
+  }
+  public void removerAluno(Aluno aluno){
+    this.listaDeAlunos.remove(aluno);
+  }
+  ///////// Metodos de manipulação de avaliações /////////
+  public void adicionarAvaliacao(Assessment avaliacao){
+    this.listaDeAvaliacoes.add(avaliacao);
+  }
+  public void removerAvaliacao(Assessment avaliacao){
+    this.listaDeAvaliacoes.remove(avaliacao);
+  }
+  
+  
+}
